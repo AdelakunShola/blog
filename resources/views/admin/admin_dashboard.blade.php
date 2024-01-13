f38a882e124caa5131b8ca330a4971385cded00d
Adelakun Shola <officialadelakun@gmail.com>
2024-01-13 05:27:42 -1100

    18. refresh admin template

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -51,7 +51,7 @@
 		<!--Start Back To Top Button-->
 		  <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
 		<!--End Back To Top Button-->
-		@include('admin.body.header')
+		@include('admin.body.footer')
 	</div>
 	<!--end wrapper-->
 
