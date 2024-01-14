aec0a904700cd8fbcc3bfe252d878eec37adbff5
Adelakun Shola <officialadelakun@gmail.com>
2024-01-13 18:54:44 -1100

    24. Admin profile change password part 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -67,8 +67,16 @@ class AdminController extends Controller
 
       return redirect()->back()->with($notification);
 
+    }//end method
 
 
+
+    public function AdminChangePassword(){
+
+      $id = Auth::user()->id;
+      $profileData = User::find($id);
+
+      return view('admin.admin_change_password',compact('profileData'));
 
 
 
--- resources/views/admin/admin_change_password.blade.php
+++ resources/views/admin/admin_change_password.blade.php
@@ -8,13 +8,13 @@
 <div class="page-content">
 				<!--breadcrumb-->
 				<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-					<div class="breadcrumb-title pe-3">User Profile</div>
+					<div class="breadcrumb-title pe-3">Change Password</div>
 					<div class="ps-3">
 						<nav aria-label="breadcrumb">
 							<ol class="breadcrumb mb-0 p-0">
 								<li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
 								</li>
-								<li class="breadcrumb-item active" aria-current="page">User Profile</li>
+								<li class="breadcrumb-item active" aria-current="page">Change Password</li>
 							</ol>
 						</nav>
 					</div>
@@ -56,42 +56,56 @@
 									<div class="card-body">
                                         <form action="{{ route('admin.profile.store') }}"  method="post"  enctype="multipart/form-data" >
                                             @csrf
+
+
 										<div class="row mb-3">
 											<div class="col-sm-3">
-												<h6 class="mb-0">Full Name</h6>
+												<h6 class="mb-0">Old Password</h6>
 											</div>
 											<div class="col-sm-9 text-secondary">
-												<input type="text" name="name" class="form-control" value="{{ $profileData->name }}" />
-											</div>
-										</div>
-										<div class="row mb-3">
-											<div class="col-sm-3">
-												<h6 class="mb-0">Email</h6>
-											</div>
-											<div class="col-sm-9 text-secondary">
-												<input type="text" name="email" class="form-control" value="{{ $profileData->email }}" />
-											</div>
-										</div>
-										
-										<div class="row mb-3">
-											<div class="col-sm-3">
-												<h6 class="mb-0">Photo</h6>
-											</div>
-											<div class="col-sm-9 text-secondary">
-												<input type="file" name="photo" class="form-control" id="image" />
+												<input type="password" name="old_password" class="form-control @error('old_password') is-invalid @enderror" />
+
+                                                @error('old_password')
+
+                                                <span class="text-danger"> {{$message}} </span>
+                                                @enderror
 											</div>
 										</div>
 
 
                                         <div class="row mb-3">
 											<div class="col-sm-3">
-												<h6 class="mb-0"> </h6>
+												<h6 class="mb-0">New Password</h6>
 											</div>
 											<div class="col-sm-9 text-secondary">
-                                            <img id="showImage" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
+												<input type="password" name="new_password" class="form-control @error('new_password') is-invalid @enderror" />
+
+                                                @error('new_password')
+
+                                                <span class="text-danger"> {{$message}} </span>
+                                                @enderror
 											</div>
 										</div>
 
+
+                                        <div class="row mb-3">
+											<div class="col-sm-3">
+												<h6 class="mb-0">Confirm Password</h6>
+											</div>
+											<div class="col-sm-9 text-secondary">
+												<input type="password" name="new_password" class="form-control" />
+
+                                              
+											</div>
+										</div>
+
+
+										
+										
+										
+
+
+                                      
 
 										<div class="row">
 											<div class="col-sm-3"></div>
@@ -111,18 +125,6 @@
 
 
 
-    <script type="text/javascript" >
-    $(document).ready(function(){
-        $('#image').change(function(e){
-            var reader = new FileReader();
-            reader.onload = function(e){
-                $('#showImage').attr('src',e.target.result);
-            }
-            reader.readAsDataURL(e.target.files['0']);
-
-    });
-});
-</script>
 
 
 @endsection
