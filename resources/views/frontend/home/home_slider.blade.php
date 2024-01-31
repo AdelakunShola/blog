53c83aa566e45d9e1e6028b083de8cb53df2007d
Adelakun Shola <officialadelakun@gmail.com>
2024-01-31 02:36:29 -1100

    frontend popular, featured and slider part 3

--- resources/views/frontend/home/home_slider.blade.php
+++ resources/views/frontend/home/home_slider.blade.php
@@ -1,3 +1,9 @@
+@php
+
+$slider = App\Models\BlogPost::where('slider',1)->get();
+
+@endphp
+
 <h1 class="d-none">Home Default Blog</h1>
         <div class="slider-area bg-color-grey">
             <div class="axil-slide slider-style-1">
@@ -5,12 +11,15 @@
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="slider-activation axil-slick-arrow">
+
+
+                                 @foreach($slider as $item)
                                 <!-- Start Single Slide  -->
                                 <div class="content-block">
                                     <!-- Start Post Thumbnail  -->
                                     <div class="post-thumbnail">
                                         <a href="post-details.html">
-                                            <img src="{{asset('frontend/assets/images/post-images/gallery-post-01.jpg')}}" alt="Post Images">
+                                            <img src="{{ asset($item->post_image)}}" alt="Post Images" style="height: 600px;">
                                         </a>
                                     </div>
                                     <!-- End Post Thumbnail  -->
@@ -21,40 +30,35 @@
                                             <div class="post-cat-list">
                                                 <a class="hover-flip-item-wrapper" href="#">
                                                     <span class="hover-flip-item">
-                                                        <span data-text="DESIGN">DESIGN</span>
+                                                        <span data-text="{{$item['blog']['category_name']}}">{{$item['blog']['category_name']}}</span>
                                                     </span>
                                                 </a>
                                             </div>
                                         </div>
-                                        <h2 class="title"><a href="post-details.html">4 types of research methods all designers should know</a></h2>
+                                        <h2 class="title"><a href="{{ url('blog/details/'.$item->post_slug) }}">{{ $item->post_title }}</a></h2>
                                         <!-- Post Meta  -->
                                         <div class="post-meta-wrapper with-button">
                                             <div class="post-meta">
                                                 <div class="post-author-avatar border-rounded">
-                                                    <img src="{{asset('frontend/assets/images/post-images/author/author-image-3.png')}}" alt="Author Images">
+                                                    <img src="{{$item['user']['photo']}}" alt="Author Images">
                                                 </div>
                                                 <div class="content">
                                                     <h6 class="post-author-name">
                                                         <a class="hover-flip-item-wrapper" href="author.html">
                                                             <span class="hover-flip-item">
-                                                                <span data-text="Rahabi Khan">Rahabi Khan</span>
+                                                                <span data-text="{{$item->user->name}}">{{$item['user']['name']}}</span>
                                                             </span>
                                                         </a>
                                                     </h6>
                                                     <ul class="post-meta-list">
-                                                        <li>Feb 17, 2019</li>
-                                                        <li>300k Views</li>
+                                                        <li>{{ $item->created_at->format('M d Y') }}</li>
+                                                    
                                                     </ul>
                                                 </div>
                                             </div>
-                                            <ul class="social-share-transparent justify-content-end">
-                                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                                <li><a href="#"><i class="fas fa-link"></i></a></li>
-                                            </ul>
+                                           
                                             <div class="read-more-button cerchio">
-                                                <a class="axil-button button-rounded hover-flip-item-wrapper" href="post-details.html">
+                                                <a class="axil-button button-rounded hover-flip-item-wrapper" href="{{ url('blog/details/'.$item->post_slug) }}">
                                                     <span class="hover-flip-item">
                                                         <span data-text="Read Post">Read Post</span>
                                                     </span>
@@ -65,131 +69,9 @@
                                     <!-- End Post Content  -->
                                 </div>
                                 <!-- End Single Slide  -->
+                                @endforeach
 
-                                <!-- Start Single Slide  -->
-                                <div class="content-block">
-                                    <!-- Start Post Thumbnail  -->
-                                    <div class="post-thumbnail">
-                                        <a href="post-details.html">
-                                            <img src="{{asset('frontend/assets/images/post-images/gallery-post-03.jpg')}}" alt="Post Images">
-                                        </a>
-                                    </div>
-                                    <!-- End Post Thumbnail  -->
-                                    <!-- Start Post Content  -->
-                                    <div class="post-content">
-                                        <div class="post-cat">
-                                            <div class="post-cat-list">
-                                                <a class="hover-flip-item-wrapper" href="#">
-                                                    <span class="hover-flip-item">
-                                                        <span data-text="CAREERS">CAREERS</span>
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        </div>
-                                        <h2 class="title"><a href="post-details.html">These 5 tips will help you nail your next design presentation</a></h2>
-
-                                        <!-- Post Meta  -->
-                                        <div class="post-meta-wrapper with-button">
-                                            <div class="post-meta">
-                                                <div class="post-author-avatar border-rounded">
-                                                    <img src="{{asset('frontend/assets/images/post-images/author/author-image-1.png')}}" alt="Author Images">
-                                                </div>
-                                                <div class="content">
-                                                    <h6 class="post-author-name">
-                                                        <a class="hover-flip-item-wrapper" href="author.html">
-                                                            <span class="hover-flip-item">
-                                                                <span data-text="Syed Tahmim">Syed Tahmim</span>
-                                                            </span>
-                                                        </a>
-
-                                                    </h6>
-                                                    <ul class="post-meta-list">
-                                                        <li>Feb 17, 2019</li>
-                                                        <li>300k Views</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <ul class="social-share-transparent justify-content-end">
-                                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                                <li><a href="#"><i class="fas fa-link"></i></a></li>
-                                            </ul>
-                                            <div class="read-more-button cerchio">
-                                                <a class="axil-button button-rounded hover-flip-item-wrapper" href="post-details.html">
-                                                    <span class="hover-flip-item">
-                                                        <span data-text="Read Post">Read Post</span>
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- End Post Content  -->
-                                </div>
-                                <!-- End Single Slide  -->
-
-                                <!-- Start Single Slide  -->
-                                <div class="content-block">
-                                    <!-- Start Post Thumbnail  -->
-                                    <div class="post-thumbnail">
-                                        <a href="post-details.html">
-                                            <img src="{{asset('frontend/assets/images/post-images/gallery-post-02.jpg')}}" alt="Post Images">
-                                        </a>
-                                    </div>
-                                    <!-- End Post Thumbnail  -->
-                                    <!-- Start Post Content  -->
-                                    <div class="post-content">
-                                        <div class="post-cat">
-                                            <div class="post-cat-list">
-                                                <a class="hover-flip-item-wrapper" href="#">
-                                                    <span class="hover-flip-item">
-                                                        <span data-text="DESIGN">DESIGN</span>
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        </div>
-                                        <h2 class="title"><a href="post-details.html">iPadOS 14 introduces
-                                                new designed specifically for iPad</a></h2>
-
-                                        <!-- Post Meta  -->
-                                        <div class="post-meta-wrapper with-button">
-                                            <div class="post-meta">
-                                                <div class="post-author-avatar border-rounded">
-                                                    <img src="{{asset('frontend/assets/images/post-images/author/author-image-2.png')}}" alt="Author Images">
-                                                </div>
-                                                <div class="content">
-                                                    <h6 class="post-author-name">
-                                                        <a class="hover-flip-item-wrapper" href="author.html">
-                                                            <span class="hover-flip-item">
-                                                                <span data-text="Ismat Jahan">Ismat Jahan</span>
-                                                            </span>
-                                                        </a>
-
-                                                    </h6>
-                                                    <ul class="post-meta-list">
-                                                        <li>Feb 17, 2019</li>
-                                                        <li>300k Views</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <ul class="social-share-transparent justify-content-end">
-                                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                                <li><a href="#"><i class="fas fa-link"></i></a></li>
-                                            </ul>
-                                            <div class="read-more-button cerchio">
-                                                <a class="axil-button button-rounded hover-flip-item-wrapper" href="post-details.html">
-                                                    <span class="hover-flip-item">
-                                                        <span data-text="Read Post">Read Post</span>
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- End Post Content  -->
-                                </div>
-                                <!-- End Single Slide  -->
+                                
 
                             </div>
                         </div>
